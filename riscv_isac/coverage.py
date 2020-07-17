bf3b3ef0205d3815aa136ce4b9f70d589c5c1c02
Neel Gala <neelgala@incoresemi.com>
2020-07-17 11:19:24 +0530

    changes to use the new compliance.cgf file

--- riscv_isac/coverage.py
+++ riscv_isac/coverage.py
@@ -3,8 +3,9 @@ from ruamel.yaml import YAML
 import riscv_isac.parsers as helpers
 from collections import Counter
 import sys
-yaml = YAML(typ="rt")
-yaml.default_flow_style = False
+yaml = YAML(typ="safe")
+yaml.default_flow_style = True
+yaml.explicit_start = True
 yaml.allow_unicode = True
 
 
@@ -27,55 +28,51 @@ def pretty_print_regfile(regfile):
                 'x'+str(index+3) + ' : ' + str(regfile[index+3]) + '\t' )
     print('\n\n')
 
-def add_cov_points(cgf):
-    for key,value in cgf.items():
-        cov_dict = {}
-        for nodes in value:
-            if nodes != 'config' and nodes != 'opcode':
-                length = len(value[nodes])
-                init_cov = [0]*length
-                cov_dict[nodes+'_cov'] = init_cov
-        cgf[key].update(cov_dict)
-    return cgf
-
 def gen_report(cgf, detailed):
     rpt_str = ''
-    for cov_points, value in cgf.items():
-        rpt_str += cov_points + ':\n'
-        total_uncovered = 0
-        total_nodes = 0
-        for nodes in value:
-            if nodes != 'config' and nodes != 'opcode' and '_cov' not in nodes:
-                total_uncovered += value[nodes+'_cov'].count(0)
-                total_nodes += len(value[nodes])
-        rpt_str += '  coverage: '+str(total_nodes -total_uncovered) + \
-                '/' + str(total_nodes)+'\n'
-        for nodes in value:
-            if nodes != 'config' and nodes != 'opcode' and '_cov' not in nodes:
-                uncovered = value[nodes+'_cov'].count(0)
-                percentage_covered = str((len(value[nodes]) - uncovered)/len(value[nodes]))
-                node_level_str =  '  ' + nodes + ':\n'
-                node_level_str += '    coverage: ' + \
-                        str(len(value[nodes]) - uncovered) + \
-                        '/' + str(len(value[nodes]))
-                rpt_str += node_level_str + '\n'
-                if detailed:
-                    rpt_str += '    detail:\n'
-                    for x in range(len(value[nodes])):
-                        rpt_str += '      - '+str(value[nodes][x]) + ': ' + str(value[nodes+'_cov'][x]) + '\n'
+    for cov_labels, value in cgf.items():
+        if 'cov' in cov_labels:
+            rpt_str += cov_labels + ':\n'
+            total_uncovered = 0
+            total_categories = 0
+            for categories in value:
+                if categories != 'config' and categories != 'opcode':
+                    for coverpoints, coverage in value[categories].items():
+                        if coverage == 0:
+                            total_uncovered += 1
+                    total_categories += len(value[categories])
+            rpt_str += '  coverage: '+str(total_categories -total_uncovered) + \
+                    '/' + str(total_categories)+'\n'
+            for categories in value:
+                if categories != 'config' and categories != 'opcode':
+                    uncovered = 0
+                    for coverpoints, coverage in value[categories].items():
+                        if coverage == 0:
+                            uncovered += 1
+                    percentage_covered = str((len(value[categories]) - uncovered)/len(value[categories]))
+                    node_level_str =  '  ' + categories + ':\n'
+                    node_level_str += '    coverage: ' + \
+                            str(len(value[categories]) - uncovered) + \
+                            '/' + str(len(value[categories]))
+                    rpt_str += node_level_str + '\n'
+                    if detailed:
+                        rpt_str += '    detail:\n'
+                        for coverpoints in value[categories]:
+                            rpt_str += '      - '+str(coverpoints) + ': ' + str(value[categories][coverpoints]) + '\n'
     return rpt_str
 
 def merge_coverage(files, cgf_file, detailed):
     with open(cgf_file, "r") as file:
-        cgf = add_cov_points(yaml.load(file))
+        cgf = yaml.load(file)
     for logs in files:
         with open(logs, "r") as file:
             logs_cov = yaml.load(file)
-        for key, value in cgf.items():
-            for nodes in value:
-                if '_cov' in nodes:
-                    for x in range(len(value[nodes])):
-                        cgf[key][nodes][x] += logs_cov[key][nodes][x]
+        for cov_labels, value in logs_cov.items():
+            for categories in value:
+                if categories != 'config' and categories != 'opcode':
+                    for coverpoints, coverage in value[categories].items():
+                        if coverpoints in cgf[cov_labels][categories]:
+                            cgf[cov_labels][categories][coverpoints] += coverage
     return gen_report(cgf, detailed)
 
 def twos_complement(val,bits):
@@ -122,37 +119,23 @@ def compute_per_line(line, cgf, mode, xlen, regfile, saddr, eaddr):
             rd_val = twos_complement(int(commitvalue[2],16), xlen)
         else:
             rd_val = 0
-        for key,value in cgf.items():
-            if instr.instr_name == value['opcode']:
-                if 'rs1' in value and 'x'+str(rs1) in value['rs1']:
-                    index = value['rs1'].index('x'+str(rs1))
-                    cgf[key]['rs1_cov'][index] += 1
-                if 'rs2' in value and 'x'+str(rs2) in value['rs2']:
-                    index = value['rs2'].index('x'+str(rs2))
-                    cgf[key]['rs2_cov'][index] += 1
-                if 'rd' in value and 'x'+str(rd) in value['rd']:
-                    index = value['rd'].index('x'+str(rd))
-                    cgf[key]['rd_cov'][index] += 1
-                if 'imm_val' in value and imm_val in value['imm_val']:
-                    index = value['imm_val'].index(imm_val)
-                    cgf[key]['imm_val_cov'][index] += 1
-                if 'rs1_val' in value and rs1_val in value['rs1_val']:
-                    index = value['rs1_val'].index(rs1_val)
-                    cgf[key]['rs1_val_cov'][index] += 1
-                if 'rs2_val' in value and rs2_val in value['rs2_val']:
-                    index = value['rs2_val'].index(rs2_val)
-                    cgf[key]['rs2_val_cov'][index] += 1
-                if 'rd_val' in value and rd_val in value['rd_val']:
-                    index = value['rd_val'].index(rd_val)
-                    cgf[key]['rd_val_cov'][index] += 1
-                if 'op_comb' in value and len(value['op_comb']) != 0 :
-                    for x in range(len(value['op_comb'])):
-                        if eval(value['op_comb'][x]):
-                            cgf[key]['op_comb_cov'][x] += 1
-                if 'val_comb' in value and len(value['val_comb']) != 0 :
-                    for x in range(len(value['val_comb'])):
-                        if eval(value['val_comb'][x]):
-                            cgf[key]['val_comb_cov'][x] += 1
+        for cov_labels,value in cgf.items():
+            if 'cov' in cov_labels:
+                if instr.instr_name == value['opcode']:
+                    if 'rs1' in value and 'x'+str(rs1) in value['rs1']:
+                        value['rs1']['x'+str(rs1)] += 1
+                    if 'rs2' in value and 'x'+str(rs2) in value['rs2']:
+                        value['rs2']['x'+str(rs2)] += 1
+                    if 'rd' in value and 'x'+str(rd) in value['rd']:
+                        value['rd']['x'+str(rd)] += 1
+                    if 'op_comb' in value and len(value['op_comb']) != 0 :
+                        for coverpoints in value['op_comb']:
+                            if eval(coverpoints):
+                                cgf[cov_labels]['op_comb'][coverpoints] += 1
+                    if 'val_comb' in value and len(value['val_comb']) != 0 :
+                        for coverpoints in value['val_comb']:
+                            if eval(coverpoints):
+                                cgf[cov_labels]['val_comb'][coverpoints] += 1
 
         if commitvalue is not None:
             regfile[int(commitvalue[1])] =  int(commitvalue[2], 16)
@@ -169,7 +152,7 @@ def compute(trace_file, cgf_file, mode, merge_cov, detailed, xlen, saddr,
         return merge_coverage(merge_cov, cgf_file, detailed)
     else:
         with open(cgf_file, "r") as file:
-            cgf = add_cov_points(yaml.load(file))
+            cgf = yaml.load(file)
 
         with open(trace_file) as fp:
             for line in fp:
