17ff031e5fc27ffcdf1fab8b75de43327e032e52
dracarys99 <spawan1999@gmail.com>
2020-07-22 17:40:45 +0530

    Added twos complement and integers for normalized cgf.

--- riscv_isac/cgf_normalize.py
+++ riscv_isac/cgf_normalize.py
@@ -1,27 +1,42 @@
-import copy
-def walking_ones(var, size):
-    dataset = [1 << exp for exp in range(size)]
-    coverpoints = [var + ' == ' + str(hex(d)) for d in dataset]
+def twos(val,bits):
+    if isinstance(val,str):
+        if '0x' in val:
+            val = int(val,16)
+        else:
+            val = int(val,2)
+    if (val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return val
+
+def walking_ones(var, size,signed=True):
+    if not signed:
+        dataset = [1 << exp for exp in range(size)]
+    else:
+        dataset = [twos(1 << exp,size) for exp in range(size)]
+    coverpoints = [var + ' == ' + str(d) for d in dataset]
     return coverpoints
 
-def walking_zeros(var, size):
-    mask = hex(2**size -1)
-    dataset = [(1 << exp)^(2**size -1) for exp in range(size)]
-    coverpoints = [var + ' == ' + str(hex(d)) for d in dataset]
+def walking_zeros(var, size,signed=True):
+    mask = 2**size -1
+    if not signed:
+        dataset = [(1 << exp)^mask for exp in range(size)]
+    else:
+        dataset = [twos((1 << exp)^mask,size) for exp in range(size)]
+    coverpoints = [var + ' == ' + str(d) for d in dataset]
     return coverpoints
 
 def expand_cgf(cgf, xlen):
-    new_cgf = copy.deepcopy(cgf)
     for labels, cats in cgf.items():
         if labels != 'datasets':
             for label,node in cats.items():
                 if 'abstract_comb' in node:
-                    del new_cgf[labels][label]['abstract_comb']
-                    for coverpoints, coverage in cats[label]['abstract_comb'].items():
+                    temp = cgf[labels][label]['abstract_comb']
+                    del cgf[labels][label]['abstract_comb']
+                    for coverpoints, coverage in temp.items():
                             if 'walking' in coverpoints:
                                 exp_cp = eval(coverpoints)
                                 for e in exp_cp:
-                                    new_cgf[labels][label][e] = coverage
-    return new_cgf
+                                    cgf[labels][label][e] = coverage
+    return cgf
 
 
