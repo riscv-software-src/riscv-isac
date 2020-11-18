ded91538330628f93a50f426b5055e296ac33275
Neel Gala <neelgala@incoresemi.com>
2020-11-18 19:35:10 +0530

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     name='riscv_isac',
-    version='0.4.0',
+    version='0.5.0',
     description="RISC-V ISAC",
     long_description=readme + '\n\n',
     classifiers=[
