336eb2e56df0ff853c9d1ead65edd61544e8f570
Preetham <aripreetham@gmail.com>
2021-02-21 10:03:50 +0530

    Added fp_dataset function support

--- riscv_isac/cgf_normalize.py
+++ riscv_isac/cgf_normalize.py
@@ -2,6 +2,7 @@
 from math import *
 import riscv_isac.utils as utils
 import itertools
+from riscv_isac.fp_dataset import *
 
 def twos(val,bits):
     '''
@@ -178,7 +179,7 @@ def expand_cgf(cgf_files, xlen):
                         temp = cgf[labels][label]['abstract_comb']
                         del cgf[labels][label]['abstract_comb']
                         for coverpoints, coverage in temp.items():
-                                if 'walking' in coverpoints or 'alternate' in coverpoints or 'sp_dataset' in coverpoints:
+                                if 'walking' in coverpoints or 'alternate' in coverpoints or 'sp_dataset' in coverpoints or 'b1' in coverpoints:
                                     exp_cp = eval(coverpoints)
                                     for e in exp_cp:
                                         cgf[labels][label][e] = coverage
