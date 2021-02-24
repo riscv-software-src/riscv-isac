abc964f969ce107dadc5a3189342c38e35dbfd7a
Neel Gala <neelgala@incoresemi.com>
2021-02-24 22:51:21 +0530

    created dataset for f
    improved b1 function generation

--- riscv_isac/fp_dataset.py
+++ riscv_isac/fp_dataset.py
@@ -1,34 +1,69 @@
 from riscv_isac.log import logger
+import itertools
+import random
+import sys
 
-def sem_return(hex_list):
-	
-	sem_list=[]
-	for i in range(len(hex_list)):
-		bin_val = bin(int('1'+hex_list[i][2:],16))[3:]
-		sgn = bin_val[0]
-		exp = bin_val[1:9]
-		mant = bin_val[9:]
-		sem_list.append('fs1 == '+ sgn +' and fe1 == '+ '0x'+hex(int('1111'+exp,2))[3:] +' and fm1 == '+ '0x'+hex(int('11110'+mant,2))[3:])
-	return(sem_list)
-	
-def b1(flen,ops):
-	
-	cc_list = ['0x00000000','0x80000000'] #,'0x3F800000','0xBF800000','0x00000001','0x80000001','0x007FFFFF','0x807FFFFF','0x00800000','0x80800000','0x7F7FFFFF','0xFF7FFFFF','0x7F800000','0xFF800000','0xFFFFFFFF','0xFF800001','0xFF8FFFFF']
-	sn_list = ['0x00000001','0x00000002'] #,'0x00000003','0x00000004','0x00000005','0x80000001','0x80000002','0x80000003','0x80000004','0x80000005']
-	nn_list = ['0x00800001','0x00800002'] #,'0x00800003','0x00800004','0x00800005','0x80800001','0x80800002','0x80800003','0x80800004','0x80800005']
-	rm_list = ['0'] #,'1','2','3','4']
-	
-	f_list = sem_return(cc_list + sn_list + nn_list)
-	
-	if(ops == 2):
-		coverpoints = [i +' and '+ j.replace('fs1','fs2').replace('fe1','fe2').replace('fm1','fm2') +' and rm == '+ k for i in f_list for j in f_list for k in rm_list]
-	elif(ops == 1):
-		coverpoints = [i +' and rm == '+ j for i in f_list for j in rm_list]
-	elif(ops == 3):
-		coverpoints = [i +' and '+ j.replace('fs1','fs2').replace('fe1','fe2').replace('fm1','fm2') +' and '+j.replace('fs1','fs3').replace('fe1','fe3').replace('fm1','fm3') +' and rm == '+ l for i in f_list for j in f_list for k in f_list for l in rm_list]
-	mess='Generating ' + str(len(coverpoints)) + ' coverpoints using Model B1!'
-	logger.info(mess)
-	return coverpoints
+fzero       = ['0x00000000', '0x80000000']
+fminsubnorm = ['0x00000001', '0x80000001']
+fsubnorm    = ['0x00000002', '0x80000002', '0x007FFFFE', '0x807FFFFE', '0x00555555', '0x80555555']
+fmaxsubnorm = ['0x007FFFFF', '0x807FFFFF']
+fminnorm    = ['0x00800000', '0x80800000']
+fnorm       = ['0x00800001', '0x80800001', '0x00855555', '0x80855555', '0x008AAAAA', '0x808AAAAA', '0x55000000', '0xD5000000','0x2A000000', '0xAA000000' ]
+fmaxnorm    = ['0x7F7FFFFF', '0xFF7FFFFF']
+finfinity   = ['0x7F800000', '0xFF800000']
+fdefaultnan = ['0x7FC00000', '0xFFC00000']
+fqnan       = ['0x7FC00001', '0xFFC00001', '0x7FC55555', '0xFFC55555']
+fsnan       = ['0x7F800001', '0xFF800001', '0x7FAAAAAA', '0xFFAAAAAA']
+fone        = ['0x3F800000', '0xBF800000']
+rounding_modes = ['0','1','2','3','4']
+
+def extract_fields(flen, hexstr, postfix):
+    if flen == 32:
+        e_sz = 8
+        m_sz = 23
+    else:
+        e_sz = 11
+        m_sz = 52
+    bin_val = bin(int('1'+hexstr[2:],16))[3:]
+    sgn = bin_val[0]
+    exp = bin_val[1:e_sz+1]
+    man = bin_val[e_sz+1:]
+
+    string = 'fs'+postfix+' == '+str(sgn) +\
+            ' and fe'+postfix+' == '+str(hex(int(exp,2))) +\
+            ' and fm'+postfix+' == '+str(hex(int(man,2)))
+
+    return string
+
+def ibm_b1(flen, ops):
+    '''
+    Test all combinations of floating-point basic types, positive and negative, for
+    each of the inputs. The basic types are Zero, One, MinSubNorm, SubNorm,
+    MaxSubNorm, MinNorm, Norm, MaxNorm, Infinity, DefaultNaN, QNaN, and
+    SNaN.
+    '''
+    if flen == 32:
+        basic_types = fzero + fminsubnorm + [fsubnorm[0], fsubnorm[3]] +\
+            fmaxsubnorm + fminnorm + [fnorm[0], fnorm[3]] + fmaxnorm + \
+            finfinity + fdefaultnan + [fqnan[0], fqnan[3]] + \
+            [fsnan[0], fsnan[3]] + fone
+    else:
+        logger.error('D part is missing')
+        sys.exit(1)
+
+    # the following creates a cross product for ops number of variables
+    b1_comb = list(itertools.product(*ops*[basic_types]))
+    coverpoints = []
+    for c in b1_comb:
+        cvpt = ""
+        for x in range(1, ops+1):
+#            cvpt += 'rs'+str(x)+'_val=='+str(c[x-1]) # uncomment this if you want rs1_val instead of individual fields
+            cvpt += (extract_fields(flen,c[x-1],str(x)))
+            cvpt += " and "
+        cvpt += "rm == 0"
+        coverpoints.append(cvpt)
+
+    return coverpoints
 
 def b2(flen,ops):
 	
