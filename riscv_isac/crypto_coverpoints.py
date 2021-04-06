164c294d6e3e731e38982f75ff8df1d1ab4843a3
Neel Gala <neelgala@incoresemi.com>
2021-04-06 10:23:41 +0530

    updated uniform random to be more generic

--- riscv_isac/crypto_coverpoints.py
+++ riscv_isac/crypto_coverpoints.py
@@ -26,34 +26,35 @@ def test_pattern1():
 	
 	return(coverpoints)
 	
-def test_pattern2(N=1, seed=-1, l_rs1=-1, u_rs1=-1, l_rs2=-1, u_rs2=-1):
+def test_pattern2(N=10, seed=10, variables=['rs1','rs2','bs'], size=[32,32,1]):
 	'''
 	Test pattern 2: Uniform Random
 	Generate uniform random values for rs1, rs2 and bs.
 	Let register values be un-constrained: 0..31.
 	Repeat N times for each instruction until sufficient coverage is reached.
+
+        :param N: Number of random combinations to be generated
+        :param seed: intial seed value of the random library
+        :param variables: list of string variables indicating the operands
+        :param size: list of bit-sizes of each variable defined in variables.
+        
+        :type N: int
+        :type seed: int
+        :type variables: List[str]
+        :type size: List[int]
+
 	'''
-	if seed == -1:
-		random.seed(0)
-	else:
-		random.seed(seed)
-	
-	if l_rs1 == -1:
-		l_rs1 = 0
-	if u_rs1 == -1:
-		u_rs1 = pow(2,32)
-	if l_rs2 == -1:
-		l_rs2 = 0
-	if u_rs2 == -1:
-		u_rs2 = pow(2,32)
+	random.seed(seed)
 	
 	coverpoints = []
 	while N!= 0:
-		rs1 = int(random.uniform(l_rs1, u_rs1))
-		rs2 = int(random.uniform(l_rs2, u_rs2))
-		bs = random.randrange(5)
-		coverpoints.append('rs1 == '+ "0x{:08x}".format(rs1) + ' and rs2 == '+ "0x{:08x}".format(rs2) + ' and bs == '+ str(bs))
-		N = N-1
+            random_vals = []
+            for v in range(len(variables)):
+                val = int(random.uniform(0,2**size[v]))
+                random_vals.append(variables[v] + \
+                        ' == {0:#0{1}x}'.format(val,int(size[v]/4)+2))
+            coverpoints.append(" and ".join(random_vals))
+            N = N-1
 	
 	return(coverpoints)
 
