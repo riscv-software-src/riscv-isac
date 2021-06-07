84ef6126bed3e4d8e669c5cf1daa1a95e1a2c092
ShrreyaSingh <71180881+ShrreyaSingh@users.noreply.github.com>
2021-06-07 22:26:30 +0530

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,42 @@
+
+import importlib
+import pluggy
+import riscv_isac.plugins.specification as spec
+# import specification as spec
+
+def interface (trace, arch, mode):
+    parser_pm = pluggy.PluginManager("parser")
+    decoder_pm = pluggy.PluginManager("decoder")
+    parser_pm.add_hookspecs(spec.ParserSpec)
+    decoder_pm.add_hookspecs(spec.DecoderSpec)
+
+    parserfile = importlib.import_module("newparser_"+mode) # Parser file
+    parserclass = getattr(parserfile, "mode_"+mode) # Class
+    parser_pm.register(parserclass)
+    parser = (parser_pm.hook)(trace,arch)
+
+    instructionObjectfile = importlib.import_module("newInstruction_plugin") # Instruction file
+    decoderclass = getattr(instructionObjectfile, "Plugin_dp")  # Instruction Class
+    decoder_pm.register(decoderclass)
+    decoder = (decoder_pm.hook)(arch)
+
+    for instr, mnemonic, addr, commitvalue in parser.instruction_stream(): # Instrcution_stream yields the given values.
+        instrObj = decoder.decode(instr, addr) # decode is a fn in Instruction class that returns an InstrObj for given instr, addr.
+    
+
+    # parserfile = importlib.import_module("newparser_"+mode) # Parser file
+    # parserclass = getattr(parserfile, "mode_"+mode)(trace, arch) # Class
+
+    # instructionObjectfile = importlib.import_module("newInstruction_plugin") # Instruction file
+    # decoder = getattr(instructionObjectfile, "Plugin_dp")(arch)  # Instruction Class
+
+    # for instr, mnemonic, addr, commitvalue in parserclass.instruction_stream(): # Instrcution_stream yields the given values.
+    #     instrObj = decoder.decode(instr, addr) # decode is a fn in Instruction class that returns an InstrObj for given instr, addr.
+
+log_file_path = r"C:\Users\Shrreya\OneDrive\Desktop\Courses\InCore_Internship\riscv-isac\riscv_isac\misalign1-jalr-01.log"
+# trace = open(log_file_path,"r")
+
+interface(log_file_path, "rv32i","c_sail")
+
+
+
