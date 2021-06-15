9d01995f8e882b375a4b19353e4c88bac4547f36
ShrreyaSingh <71180881+ShrreyaSingh@users.noreply.github.com>
2021-06-15 16:25:51 +0530

    Update isac.py

--- riscv_isac/isac.py
+++ riscv_isac/isac.py
@@ -1,13 +1,18 @@
-# See LICENSE.incore file for details
+import sys
+import os
 from riscv_isac.log import logger
 import riscv_isac.utils as utils
 import riscv_isac.coverage as cov
 from elftools.elf.elffile import ELFFile
 
-def isac(output_file,elf ,trace_file, cgf, mode, parser_name, decoder_name, parser_path, decoder_path, detailed, test_labels,
+def isac(output_file,elf ,trace_file, cgf, parser_name, decoder_name, parser_path, decoder_path, detailed, test_labels,
         sig_labels, dump, cov_labels, xlen, logging=False):
     test_addr = []
     sig_addr = []
+
+    sys.path.append(parser_path)
+    sys.path.append(decoder_path)
+    sys.path.append(os.path.join(os.path.dirname(__file__), "plugins"))
 
     if elf is not None :
         test_name = elf.rsplit('.', 1)[0]
@@ -31,7 +36,7 @@ def isac(output_file,elf ,trace_file, cgf, mode, parser_name, decoder_name, pars
                 sig_addr.append((start_address,end_address))
     else:
         test_name = trace_file.rsplit(',',1)[0]
-    rpt = cov.compute(trace_file, test_name, cgf, mode,parser_name, decoder_name, parser_path, decoder_path, detailed, xlen, test_addr, dump, cov_labels, sig_addr)
+    rpt = cov.compute(trace_file, test_name, cgf, parser_name, decoder_name, detailed, xlen, test_addr, dump, cov_labels, sig_addr)
     if output_file is not None and logging:
         logger.info('Coverage Report:')
         logger.info('\n\n' + rpt)
@@ -40,4 +45,3 @@ def isac(output_file,elf ,trace_file, cgf, mode, parser_name, decoder_name, pars
         utils.dump_yaml(rpt, rpt_file)
         rpt_file.close()
         logger.info('Report File Generated : ' + str(output_file))
-
