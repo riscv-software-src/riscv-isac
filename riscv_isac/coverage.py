568a2b07eb39e5139d34422d6d0dbc80a23335d4
S Pawan Kumar <pawan.kumar@incoresemi.com>
2021-06-24 12:24:40 +0530

    Fix for wrong variable name in decoder setup call.

--- riscv_isac/coverage.py
+++ riscv_isac/coverage.py
@@ -532,7 +532,7 @@ def compute(trace_file, test_name, cgf, parser_name, decoder_name, detailed, xle
     decoderclass = getattr(instructionObjectfile, "disassembler")
     decoder_pm.register(decoderclass())
     decoder = decoder_pm.hook
-    decoder.setup(arch="rv"+str(len))
+    decoder.setup(arch="rv"+str(xlen))
 
     iterator = iter(parser.__iter__()[0])
     for instr, mnemonic, addr, commitvalue in iterator:
