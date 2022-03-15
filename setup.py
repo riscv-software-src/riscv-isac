e2f0ec6513ce066fa7a4e6d03b96599807062232
Anand Kumar S <anku.anand@gmail.com>
2022-03-15 11:14:27 +0530

    bump version from 0.10.1 to 0.10.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [ ]
 
 setup(
     name='riscv_isac',
-    version='0.10.1',
+    version='0.10.2',
     description="RISC-V ISAC",
     long_description=readme + '\n\n',
     classifiers=[
