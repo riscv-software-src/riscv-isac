3c636dad60782f2a1678f6db4e1fd2528c6a52dd
MuhammadHammad001 <hammad.bashir@10xengineers.ai>
2023-11-18 07:55:32 +0500

    update to check only the register of interest for the old_fn_csr_comb

--- riscv_isac/coverage.py
+++ riscv_isac/coverage.py
@@ -1117,6 +1117,19 @@ def compute_per_line(queue, event, cgf_queue, stats_queue, cgf, xlen, flen, addr
                                                 break
                                         if is_csr_commit:
                                             for coverpoints in value['csr_comb']:
+                                                def get_key_from_value(dictionary, target_value):
+                                                    for key, value in dictionary.items():
+                                                        if value == target_value:
+                                                            return key
+                                                    return None
+                                                #check the old_csr_value only for the register of interest
+                                                pattern_csr = r'old\("([^"]+)"\)'
+                                                match = re.search(pattern_csr, coverpoints)
+                                                if match:
+                                                    required_csr = match.group(1)
+                                                    key = get_key_from_value(csr_reg_num_to_str, required_csr)
+                                                    if (instr.csr != key):
+                                                        continue
                                                 if eval(
                                                     coverpoints,
                                                     {
